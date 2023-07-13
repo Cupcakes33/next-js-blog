7b61947df39e4620abce34053829e77a1b4d462e
Cupcakes33 <rute930627@gmail.com>
2023-07-13 20:19:24 +0900

    :lipstick: Style : Calendar, Clock Icon 추가

---
+++ src/components/icons/CalendarIcon.tsx
@@ -0,0 +1,7 @@
+import {MdDateRange} from 'react-icons/md';
+
+export default function CalendarIcon() {
+  return (
+    <MdDateRange className="inline-block w-4 h-4 mr-1" />
+  )
+}
---
+++ src/components/icons/ClockIcon.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineClockCircle } from "react-icons/ai";
+
+import React from "react";
+
+export default function ClockIcon() {
+  return <AiOutlineClockCircle className="inline-block w-4 h-4 mr-1" />;
+}
